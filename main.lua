c177f228f4ccd8b462140a9beaa71df5e3b2f426
Arthur Steinbach <80470405+Osunosairou@users.noreply.github.com>
2021-11-08 10:39:32 -0300

    status change v1

--- main.lua
+++ main.lua
@@ -1,24 +1,46 @@
 local PoisonMushroom = RegisterMod("PoisonMushroom", 1);
-PoisonMushroom.COLLECTIBLE_POISON_MUSHROOM = Isaac.GetItemIdByName("Poison Mushroom");
+local game = Game();
+local POISON_MUSH = Isaac.GetItemIdByName("Poison Mushroom");
 
-function PoisonMushroom:onUpdate()
-    -- Poison Mush functionality
+local hasMush = false;
+local MUSH_DMG_BONUS = (EntityPlayer:GetEffectiveMaxHearts() * 0.50);
 
-    if Game():GetFrameCount() == 1 then
+-- Checks if player has the item
+function PoisonMushroom:updateMush(player)
+    hasMush = player:hasCollectible(POISON_MUSH);
+end
+
+-- When the run starts or continues
+function PoisonMushroom:onPlayerInit(player)
+    PoisonMushroom.updateMush(player)
+end
+
+-- Poison Mush functionality
+function PoisonMushroom:onUpdate(player)
+    -- Spawns the item at the start of the run (debug only)
+    if game:GetFrameCount() == 1 then
         PoisonMushroom.HasPoisonMushroom = false
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, PoisonMushroom.COLLECTIBLE_POISON_MUSHROOM, Vector(320,300), Vector(0,0), nill)
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE,POISON_MUSH, Vector(320,300), Vector(0,0), nill)
     end
-
-    -- Checks which players have the item
-    for playerNum = 1, Game:getNumPlayers() do
-        local player = Game():getPlayer(playerNum);
-        
-        -- Here is what the item does
-        if player:hasCollectible(PoisonMushroom.COLLECTIBLE_POISON_MUSHROOM) then
-
-        end;
-    end;
+   
 end;
 
+-- Adds the function above to callbacks
+PoisonMushroom:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, PoisonMushroom.onUpdate)
 
-PoisonMushroom:AddCallback(ModCallbacks.MC_POST_UPDATE, PoisonMushroom.onUpdate);
+-- Status effects
+function PoisonMushroom:onCache(player, cacheFlag)
+    if cacheFlag == CacheFlag.CACHE_DAMAGE then
+        if player:hasCollectible(POISON_MUSH) and not hasMush then
+            player.Damage = player.Damage + MUSH_DMG_BONUS
+        end
+    end
+    if EntityPlayer:GetSoulHearts() >= 1 then
+        Isaac.GetPlayer(0):AddMaxHearts( (-1) * (EntityPlayer:GetEffectiveMaxHearts()), true)
+    else
+        Isaac.GetPlayer(0):AddMaxHearts( (-1) * (EntityPlayer:GetEffectiveMaxHearts() - 2), true)
+    end  
+    
+end
+
+PoisonMushroom:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, PoisonMushroom.onCache)
